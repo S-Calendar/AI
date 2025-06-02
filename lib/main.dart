64a0818d19516f0e83a6767243857eaa1a1c4198
IOYX <seoyeon2julie@naver.com>
2025-06-02 14:22:16 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 // main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'pages/splash_page.dart';
 import 'pages/start_page.dart';
 import 'pages/main_page.dart';
-import 'pages/search_page.dart'; // 올바른 경로와 이름으로 수정
-
-
-
+import 'pages/search_page.dart';
+import 'settings_page.dart';
+import 'favorite_notices_page.dart';
+import 'hidden_items_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -23,11 +25,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SCalendar',
       theme: ThemeData(primarySwatch: Colors.blue),
-      initialRoute: '/', // splash부터 시작
+      initialRoute: '/',
       routes: {
         '/': (context) => const StartPage(),
         '/main_page': (context) => const MainPage(),
-        '/search': (context) => const SearchPage(), // ✅ 검색 페이지 라우트 추가
+        '/search': (context) => const SearchPage(),
+        '/settings': (context) => const SettingsPage(),
+        '/favorite': (context) => const FavoriteNoticesPage(),
+        '/hidden': (context) => const HiddenItemsPage(),        
       },
     );
   }
